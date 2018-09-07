94df3976d0578aa56dbf926fee40d3fadd5032d2
Cleyra Uzcategui <cleyra.uzcategui@gmail.com>
2018-09-07 11:01:51 +0100

    Tested Service

---
+++ src/test/java/ServiceTest.java
@@ -0,0 +1,25 @@
+import com.example.monopoly.model.Service;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ServiceTest {
+
+    Service service;
+
+    @Before
+    public void setUp(){
+        service = new Service("Kings Cross Station", "M200");
+    }
+
+    @Test
+    public void hasAName(){
+       assertEquals("Kings Cross Station", service.getName());
+    }
+
+    @Test
+    public void hasText(){
+        assertEquals("M200", service.getText());
+    }
+
+}
